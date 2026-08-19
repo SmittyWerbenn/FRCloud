c1b6d1a95dc515bc2140e60673c55291525a8269
Smitty <smitty@frcloud.id>
2026-08-19 10:32:07 +0700

    fix: add base path for GitHub Pages + remove Cloudflare plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), cloudflare()],
+  base: '/FRCloud/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
